345dc09da021b40e6ac7f942167c87e8b19e7504
Ashutoshh <ashutoshh@linux.com>
2025-08-27 08:59:51 +0530

    updates terraform

---
+++ vpc_project/all.tf
@@ -0,0 +1,91 @@
+# provider section 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  # my terraform target region to provision resources 
+  # calling variables
+  region = var.aws-region-name
+  # access_key = ""
+  # secret_key = ""
+}
+
+# variables section
+variable "aws-region-name" {
+    type = string
+    description = "region where i want to create VPC"
+  
+}
+
+variable "vpc-network" {
+    type = string 
+}
+
+variable "vpc-name" {
+    type = string
+  
+}
+
+variable "public-sb-addr" {
+    type = string
+  
+}
+
+variable "private-sb-addr" {
+    type = string
+  
+}
+
+# main section
+# step 1  create VPC 
+resource "aws_vpc" "example" {
+    cidr_block = var.vpc-network
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    tags = {
+      Name = var.vpc-name
+    }
+  
+}
+
+# step 2 creating public subnet 
+resource "aws_subnet" "public_example" {
+    vpc_id = aws_vpc.example.id
+    cidr_block = var.public-sb-addr
+    map_public_ip_on_launch = true 
+    # above line is gonna auto assign public ip to vms
+    tags = {
+      Name = "ashu-public-subnet"
+    }
+  
+}
+
+# Creating private subnet 
+
+resource "aws_subnet" "private_example" {
+    vpc_id = aws_vpc.example.id
+    cidr_block = var.private-sb-addr
+    # above line is gonna auto assign public ip to vms
+    tags = {
+      Name = "ashu-private-subnet"
+    }
+  
+}
+
+# creating IGW 
+
+resource "aws_internet_gateway" "example" {
+    vpc_id = aws_vpc.example.id
+    tags =  {
+        Name = "ashu-igw"
+    }
+  
+}
+
