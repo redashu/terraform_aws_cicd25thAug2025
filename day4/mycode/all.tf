907ece6182a254f5335d6891c2f59b3213160bd4
Ashutoshh <ashutoshh@linux.com>
2025-08-28 12:05:59 +0530

    updates

--- day4/mycode/all.tf
+++ day4/mycode/all.tf
@@ -88,7 +88,68 @@ resource "aws_vpc_security_group_egress_rule" "example-allipv6" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
+# creating ec2
 
+# RSA key of size 4096 bits
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# use above to put public in ec2 
+resource "aws_key_pair" "example" {
+  key_name = "ashu111-key"
+  #public_key = file("/home/ec2-user/.ssh/ashu-key.pub")
+  public_key = tls_private_key.example.public_key_openssh
+  
+}
+
+# incase you want to save private key as well
+resource "local_file" "ashu-private-key" {
+  content  = tls_private_key.example.private_key_pem
+  filename = "${path.module}/ashu-privateKey.pem"
+  file_permission = "0400"
+}
+
+# creating ec2 
+resource "aws_instance" "example" {
+  ami = var.aws-ami
+  instance_type = var.aws-instances-size
+  key_name = aws_key_pair.example.key_name
+  # association of security group
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  depends_on = [ local_file.ashu-private-key ]
+
+  tags = {
+    Name = "ashu-vmlinux"
+  }
+  # file provisioner to transfer data from terraform machine to remote 
+  provisioner "file" {
+    source = "./html-sample-app"
+    destination = "/tmp/"
+    
+  }
+  # remote provisioner 
+  provisioner "remote-exec" {
+    inline = [ 
+        "mkdir  -p  ~/ashu/data/webapp",
+        "sudo yum install httpd git -y ",
+        "sudo cp -rf /tmp/html-sample-app/* /var/www/html/",
+        "sudo systemctl start httpd"
+     ]
+     
+  }
+  # defining connection details 
+     connection {
+        type = "ssh"
+        user = "ec2-user"
+        timeout = "3m"
+        host = self.public_ip
+        #private_key = aws_key_pair.example.key_name
+        private_key = file("/home/ec2-user/ashu-codes/day4-code/ashu-privateKey.pem")
+       
+     }
+}
 
 
 
@@ -106,4 +167,9 @@ output "my-vpc-id" {
 output "my-securitygroup" {
     value = "my security group name is ${aws_security_group.allow_tls.name}"
   
+}
+
+resource "local_file" "example" {
+  content  = "my vpc id IS ${data.aws_vpc.my-vpc.id}"
+  filename = "${path.module}/vpc_id.txt"
 }
