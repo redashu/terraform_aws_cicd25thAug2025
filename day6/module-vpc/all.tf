4576b9d05078612a1c909a18bd8bf1e7df0d318a
Ashutoshh <ashutoshh@linux.com>
2025-09-01 09:03:55 +0530

    updates terraform

---
+++ day6/module-vpc/all.tf
@@ -0,0 +1,138 @@
+variable "vpc-network" {
+    type = string 
+}
+
+variable "vpc-name" {
+    type = string
+  
+}
+
+variable "public-sb-addr" {
+    type = string
+  
+}
+
+variable "private-sb-addr" {
+    type = string
+  
+}
+
+# step 1  create VPC 
+resource "aws_vpc" "example" {
+    cidr_block = var.vpc-network
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    tags = {
+      Name = var.vpc-name
+    }
+  
+}
+
+# step 2 creating public subnet 
+resource "aws_subnet" "public_example" {
+    vpc_id = aws_vpc.example.id
+    cidr_block = var.public-sb-addr
+    map_public_ip_on_launch = true 
+    # above line is gonna auto assign public ip to vms
+    tags = {
+      Name = "${var.vpc-name}-public-subnet"
+    }
+  
+}
+
+# Creating private subnet 
+
+resource "aws_subnet" "private_example" {
+    vpc_id = aws_vpc.example.id
+    cidr_block = var.private-sb-addr
+    # above line is gonna auto assign public ip to vms
+    tags = {
+      Name = "${var.vpc-name}-private-subnet"
+    }
+  
+}
+
+# creating IGW 
+
+resource "aws_internet_gateway" "example" {
+    vpc_id = aws_vpc.example.id
+    tags =  {
+        Name = "${var.vpc-name}-igw"
+    }
+  
+}
+
+# Creating routing table of public subnet 
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
+  }
+  
+  tags = {
+    Name = "${var.vpc-name}-publicRoute-table"
+  }
+
+}
+
+# association of route table with public subnet
+
+resource "aws_route_table_association" "example" {
+  subnet_id = aws_subnet.public_example.id
+  route_table_id = aws_route_table.example.id
+  
+}
+
+# for NAT gw -- creating EIP
+
+resource "aws_eip" "example" {
+  domain   = "vpc"
+}
+
+# Creating natGW using EIP also interface in public subnet
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.example.id
+  subnet_id = aws_subnet.public_example.id
+
+  tags = {
+    Name = "${var.vpc-name}-natgw"
+  }
+  # optional but sometimes to check limits and condition good to put
+  depends_on = [ aws_internet_gateway.example ]
+}
+
+# creating routing table for private subnet using NATgw 
+
+resource "aws_route_table" "example-private" {
+  vpc_id = aws_vpc.example.id
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.example.id
+  }
+  
+  tags = {
+    Name = "${var.vpc-name}-privateRoute-table"
+  }
+
+}
+
+# association with private subnet 
+
+resource "aws_route_table_association" "example-private" {
+  subnet_id = aws_subnet.private_example.id
+  route_table_id = aws_route_table.example-private.id
+  
+}
+
+output "vpc_id" {
+    value = aws_vpc.example.id
+  
+}
+
+output "public_subnet_id" {
+    value = aws_subnet.public_example.id
+}
+
